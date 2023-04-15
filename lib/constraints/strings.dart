6b0f88b66adb04b336b0199e755b559356654ee9
NabinStha2 <nabinstha246@gmail.com>
2023-04-16 01:20:00 +0545

    first commit

---
+++ lib/constraints/strings.dart
@@ -0,0 +1,334 @@
+import '../pages/sections/archive/archive_data.dart';
+import '../pages/sections/projects/project_Data.dart';
+
+///note
+//please see sequence top to bottom
+//i try to use all string of this projects top to bottom like projects view
+
+String app_name = 'Nabin Shrestha';
+String gmail_address = "nabinstha246@gmail.com";
+
+//header logo
+String dark_mode_logo = "assets/logo_1.png";
+String light_mode_logo = "assets/logo_2.png";
+
+//navItems name
+String home_index = "00. ";
+String about_index = "01. ";
+String experience_index = "02. ";
+String work_index = "03. ";
+String contact_index = "04. ";
+
+String home_ = "Home";
+String about_ = "About";
+String experience_ = "Experience";
+String work_ = "Work";
+String contact_ = "Contact";
+
+//all urls of app
+String clinchTech_url = "https://clinchtech.net/";
+String blackTech_url = "https://www.blacktech.com.np/";
+String course_url = "https://github.com/NabinStha2";
+// String advertising_url = "https://talhasultan.dev";
+// String startUP_url = "https://talhasultan.dev";
+// String corporation_url = "https://talhasultan.dev";
+// String designStudio_url = "https://talhasultan.dev";
+// String launchedCourse_url = "https://talhasultan.dev";
+
+// String scout_url = "https://talhasultan.dev"; //experience
+// String apple_url = "https://talhasultan.dev"; //experience
+// String starry_url = "https://talhasultan.dev"; //experience
+// String mullenLowe_url = "https://talhasultan.dev"; //experience
+
+//project url
+String builtItem1_url = "https://talhasultan.dev";
+String builtItem2_url = "https://talhasultan.dev";
+String builtItem3_url = "https://talhasultan.dev";
+
+String builtItem1Git = "https://github.com/NabinStha2/Moments";
+String builtItem1ExtraLink = "https://github.com/NabinStha2/Moments";
+
+String builtItem2Git = "https://github.com/NabinStha2/Rojgar";
+String builtItem2ExtraLink = "https://github.com/NabinStha2/Rojgar";
+
+String builtItem3Git = "https://github.com/NabinStha2/Attendance_System";
+String builtItem3ExtraLink = "https://github.com/NabinStha2/Attendance_System";
+
+//all social link of mine
+String fb_url = 'https://www.facebook.com/profile.php?id=100006996756964';
+String twitter_url = 'https://twitter.com/nabinsh65439761';
+String instagram_url = 'https://www.instagram.com/nabinstha21/';
+String linkedin_url = 'https://www.linkedin.com/in/nabin-shrestha-80016022a/';
+String github_url = 'https://github.com/NabinStha2';
+// String fiverr_url = 'https://www.fiverr.com/';
+// String youTube_url = 'https://www.youtube.com/muhammadtalhasultan';
+
+//here all sections data
+//data for home
+String home_hi_text = "Hi, my name is";
+String home_name = "Nabin Shrestha";
+String home_title = "Flutter Developer";
+String home_description =
+    "I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences.I am a dedicated and goal-oriented person with a strong interest in programming and coding. I'm pursuing a bachelor's degree in computer engineering at the moment.";
+// String home_des_link_text = "Dev Technologies.";
+String home_button = "Check out my Portfolio!";
+
+//about me data
+String about_me_title = "About me";
+String about_me_des1 =
+    "As a Flutter Developer, I have experience developing high-quality mobile applications. I have a deep understanding of the Flutter framework and its accompanying libraries, and I am skilled in creating intuitive and responsive user interfaces. I have worked on projects that required integration with APIs for app functionalities such as push notifications, real-time updates, and data storage. With excellent problem-solving skills and a proven track record of delivering successful projects, I am committed to creating innovative solutions for clients. I am comfortable working with both iOS and Android platforms and are always eager to learn and stay up-to-date with the latest trends and technologies in mobile app development.I also have a strong background in building web applications using MongoDB, ExpressJS, ReactJS, and NodeJS.I have experience designing and implementing RESTful APIs and are skilled in working with both front-end and back-end development. ";
+
+// String about_me_des2 =
+//     "Fast-forward to today, and I’ve had the privilege of working at";
+// String about_me_des2_link1 = " an advertising agency,";
+// String about_me_des2_link2 = " a start-up,";
+// String about_me_des2_link3 = " a huge corporation,";
+// String about_me_des3 = " and";
+// String about_me_des3_link = " a student-led design studio";
+// String about_me_des4 =
+//     ". My main focus these days is building accessible, inclusive products and digital experiences at";
+// String about_me_des4_link = " Upstatement";
+// String about_me_des5 = " for a variety of clients.";
+
+// String about_me_des6 = "I also recently ";
+// String about_me_des6_link = "launched a course ";
+// String about_me_des62 =
+//     "that covers everything you need to build a web app with the Spotify API using Node & React.";
+
+String about_me_des7 =
+    "Here are a few technologies I’ve been working with recently:";
+
+//working technology
+String about_me_technology1 = "Flutter";
+String about_me_technology2 = "Dart";
+String about_me_technology3 = "Firebase";
+
+String about_me_technology4 = "JavaScript";
+String about_me_technology5 = "React";
+String about_me_technology6 = "NodeJs";
+
+//experience sections data
+String experience_title = "Where I've Worked";
+List<String> experience_list = [
+  'ClinchTech',
+  'BlackTech',
+];
+
+String experience1_title = "Flutter Developer ";
+String experience1_title_linl = "@ ClinchTech";
+String experience1_duration = "Sep 2022 - Present";
+String experience1_des1 =
+    "Developed and deployed several mobile apps using Flutter framework on both iOS and Android platforms, including an education app with payment integration";
+String experience1_des2 =
+    "Ability to work in a fast-paced environment and meet tight deadlines";
+String experience1_des3 =
+    "Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis";
+
+String experience2_title = "Flutter Developer InternShip ";
+String experience2_title_linl = "@ BlackTech";
+String experience2_duration = "April 2022 - Jun 2022";
+String experience2_des1 =
+    "Worked with a team of three designers to build a marketing website and e-commerce platform for blistabloc, an ambitious startup originating from Northeastern";
+String experience2_des2 =
+    "Conducted code reviews and implemented quality assurance processes to ensure bug-free apps before deployment";
+String experience2_des3 =
+    "Collaborated with backend developers to integrate RESTful APIs for app functionalities such as push notifications and real-time updates";
+
+// String experience3_title = "UI Engineer Co-op ";
+// String experience3_title_linl = "@ Apple";
+// String experience3_duration = "July - December 2017";
+// String experience3_des1 =
+//     "Developed and shipped highly interactive web applications for Apple Music using Ember.js";
+// String experience3_des2 =
+//     "Built and shipped the Apple Music Extension within Facebook Messenger leveraging third-party and internal APIs";
+// String experience3_des3 =
+//     "Architected and implemented the front-end of Apple Music's embeddable web player widget, which lets users log in and listen to full songs in the browser";
+// String experience3_des4 =
+//     "Contributed extensively to MusicKit.js, a JavaScript framework that allows developers to add an Apple Music player to their web apps";
+
+// String experience4_title = "Studio Developer ";
+// String experience4_title_linl = "@ Scout";
+// String experience4_duration = "January - June 2017";
+// String experience4_des1 =
+//     "Collaborated with a small team of student designers to spearhead a new brand and design system for Scout’s inaugural student-led design conference at Northeastern";
+// String experience4_des2 =
+//     "Worked closely with designers and management team to develop, document, and manage the conference’s marketing website using Jekyll, Sass, and JavaScript";
+
+// String experience5_title = "Software Engineer Co-op ";
+// String experience5_title_linl = "@ Starry";
+// String experience5_duration = "July - December 2016";
+// String experience5_des1 =
+//     "Engineered and maintained major features of Starry's customer-facing web app using ES6, Handlebars, Backbone, Marionette and CSS";
+// String experience5_des2 =
+//     "Proposed and implemented scalable solutions to issues identified with cloud services and applications responsible for communicating with Starry Station";
+// String experience5_des3 =
+//     "Interfaced with user experience designers and other developers to ensure thoughtful and coherent user experiences across Starry’s iOS and Android mobile apps";
+
+// String experience6_title = "Creative Technologist Co-op ";
+// String experience6_title_linl = "@ MullenLowe";
+// String experience6_duration = "July - December 2015";
+// String experience6_des1 =
+//     "Developed and maintained code for in-house and client websites primarily using HTML, CSS, Sass, JavaScript, and jQuery";
+// String experience6_des2 =
+//     "Manually tested sites in various browsers and mobile devices to ensure cross-browser compatibility and responsiveness";
+// String experience6_des3 =
+//     "Clients included JetBlue, Lovesac, U.S. Cellular, U.S. Department of Defense, and more";
+
+//project section data
+String built_headerTitle = "Some Things I've Built";
+String builtItem_heading = "Featured Project";
+
+String builtItem1_name = "Moments";
+String builtItem1_des =
+    "It's a social networking app where we may share photos with status comments, likes, real time chat with socket.io and other features.";
+String builtItem1_uses_platform = "Flutter Dart NodeJS MongoDB ExpressJS";
+
+String builtItem2_name = "Rojgar";
+String builtItem2_des =
+    "This is a freelancing project called ROJGAR that uses the Mern stack. It contains numerous functions such as payments, job advertising, acceptance, and many more.";
+String builtItem2_uses_platform =
+    "JavaScript ReactJs NodeJs ExpressJS MongoDB MERN-Stack Material-Ui";
+
+String builtItem3_name = "Attendance App";
+String builtItem3_des1 =
+    "Attendance for employees with finger print to save their attendance data locally.";
+// String builtItem3_des2 =
+//     "Attendance for employees with finger print to save their attendance data locally.";
+String builtItem3_uses_platform = "Flutter Dart Hive LocalStorage";
+
+//other noteworthy
+String projectTitle = "Other Noteworthy Projects";
+String archive_buttonName = "view the archive";
+
+//I created a data list of all projects using project class
+List<Project> allProjects = [
+  Project(
+      'https://talhasultan.dev',
+      'https://talhasultan.dev',
+      'Moments',
+      "It's a social networking app where we may share photos with status comments, likes, real time chat with socket.io and other features.",
+      "Flutter Dart NodeJS MongoDB ExpressJS"),
+  Project(
+      'https://talhasultan.dev',
+      'https://talhasultan.dev',
+      'Rojgar',
+      "This is a freelancing project called ROJGAR that uses the Mern stack. It contains numerous functions such as payments, job advertising, acceptance, and many more.",
+      "JavaScript ReactJs NodeJs ExpressJS MongoDB MERN-Stack Material-Ui"),
+];
+
+List<Project> moreProjects = [
+  Project(
+      'https://talhasultan.dev',
+      'https://talhasultan.dev',
+      'Attendance App',
+      'Attendance for employees with finger print to save their attendance data locally.',
+      "Flutter Dart Hive LocalStorage"),
+  Project(
+      'https://talhasultan.dev',
+      'https://talhasultan.dev',
+      'Time to Have More Fun',
+      'A single page web app for helping me choose where to travel, built with Next.js, Firebase, and Tailwind CSS',
+      'Next.js   Tailwind CSS   Firebase'),
+];
+
+String showMoreButton_name = 'Show More';
+String showLessButton_name = 'Show Less';
+
+List<Archive> archiveList = [
+  Archive(
+      '2021',
+      'Koala Health',
+      'Upstatement',
+      'Next.js·TypeScript·Redux Toolkit·Stripe·Algolia·Firebase Auth·Formik·Yup·Vercel',
+      'https://talhasultan.dev'),
+  Archive(
+      '2021',
+      'Philadelphia Inquirer Sports Scoreboards',
+      'Upstatement',
+      'Next.js·TypeScript·Redux Toolkit·Stripe·Algolia·Firebase Auth·Formik·Yup·Vercel',
+      'https://talhasultan.dev'),
+  Archive('2021', 'Vanderbilt Design System', 'Upstatement', 'Twig·Puppy·JS',
+      'https://talhasultan.dev'),
+  Archive('2021', 'Michelle Wu for Boston Grassroots Toolkit', 'Upstatement',
+      'Gatsby·Styled Components', 'https://talhasultan.dev'),
+  Archive(
+      '2021',
+      'The 19th News',
+      'Upstatement',
+      'Next.js·TypeScript·Redux Toolkit·Stripe·Algolia·Firebase Auth·Formik·Yup·Vercel',
+      'https://talhasultan.dev'),
+  Archive(
+      '2021',
+      'Northeastern CSSH',
+      'Upstatement',
+      'Next.js·TypeScript·Redux Toolkit·Stripe·Algolia·Firebase Auth·Formik·Yup·Vercel',
+      'https://talhasultan.dev'),
+  Archive(
+      '2020',
+      'Integrating Algolia Search with WordPress Multisite',
+      'Upstatement',
+      'Next.js·TypeScript·Redux Toolkit·Stripe·Algolia·Firebase Auth·Formik·Yup·Vercel',
+      'https://talhasultan.dev'),
+  Archive(
+      '2020',
+      'Time to Have More Fun',
+      '___',
+      'Next.js·TypeScript·Redux Toolkit·Stripe·Algolia·Firebase Auth·Formik·Yup·Vercel',
+      'https://talhasultan.dev'),
+  Archive(
+      '2020',
+      'Upstatement.com',
+      'Upstatement',
+      'Next.js·TypeScript·Redux Toolkit·Stripe·Algolia·Firebase Auth·Formik·Yup·Vercel',
+      'https://talhasultan.dev'),
+  Archive(
+      '2020',
+      'Building a Headless Mobile App CMS From Scratch',
+      'Upstatement',
+      'Next.js·TypeScript·Redux Toolkit·Stripe·Algolia·Firebase Auth·Formik·Yup·Vercel',
+      'https://talhasultan.dev'),
+  Archive(
+      '2019',
+      'Moms Demand Action Mobile App',
+      'Upstatement',
+      'Next.js·TypeScript·Redux Toolkit·Stripe·Algolia·Firebase Auth·Formik·Yup·Vercel',
+      'https://talhasultan.dev'),
+  Archive(
+      '2019',
+      'OctoProfile',
+      '___',
+      'Next.js·TypeScript·Redux Toolkit·Stripe·Algolia·Firebase Auth·Formik·Yup·Vercel',
+      'https://talhasultan.dev'),
+  Archive(
+      '2020',
+      'Google Keep Clone',
+      'Upstatement',
+      'Next.js·TypeScript·Redux Toolkit·Stripe·Algolia·Firebase Auth·Formik·Yup·Vercel',
+      'https://talhasultan.dev'),
+  Archive(
+      '2019',
+      'Spotify Profile',
+      'Upstatement',
+      'Next.js·TypeScript·Redux Toolkit·Stripe·Algolia·Firebase Auth·Formik·Yup·Vercel',
+      'https://talhasultan.dev'),
+  Archive(
+      '2019',
+      'Devoted Health',
+      'Upstatement',
+      'Next.js·TypeScript·Redux Toolkit·Stripe·Algolia·Firebase Auth·Formik·Yup·Vercel',
+      'https://talhasultan.dev'),
+];
+
+//section of contacts
+String contactIndexTitle = "What's Next?";
+String contact_title = "Get In Touch";
+String contact_des =
+    "I’m currently looking for any new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!";
+String contact_buttonName = "Say Hello";
+String contact_url =
+    "mailto:nabinstha246@gmail.com?subject=Contact Through Website&body=Type Message"; //put your gmail here which receive clients email
+
+//footer data
+String footerText = "Nabin Shrestha";
+String footerStarNumber = "0";
+String footerShareNumber = "0";
---
+++ lib/widgets/header.dart
@@ -0,0 +1,187 @@
+import 'package:universal_html/html.dart' as html;
+
+import 'package:client_portfolio/constraints/colors.dart';
+import 'package:client_portfolio/constraints/strings.dart';
+import 'package:client_portfolio/providers/themeProvider.dart';
+import 'package:client_portfolio/widgets/light_Dark_mode.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import '../constraints/responsive.dart';
+import 'navbar/navItem.dart';
+
+class Header extends StatelessWidget {
+  const Header({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final themeprovider = Provider.of<ThemeProvider>(context);
+    final logo_ = themeprovider.isDarkMode ? dark_mode_logo : light_mode_logo;
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 20),
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 10),
+        child: Row(
+          children: <Widget>[
+            //here is my header app logo
+            Expanded(
+              flex: 1,
+              child: isMobile(context)
+                  ? Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: Image.asset(
+                        logo_,
+                        width: 50,
+                        height: 50,
+                      ),
+                    )
+                  : Image.asset(
+                      logo_,
+                      width: 50,
+                      height: 50,
+                    ),
+            ),
+
+            //when device is tab or desktop then the nav menu item is show
+            if (!isMobile(context))
+              Expanded(
+                flex: 6,
+                child: Padding(
+                  padding: EdgeInsets.only(right: isTab(context) ? 30 : 60),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Light_Dark_Mode(), //to change light&dark mode i use it
+                      // NavItem(
+                      //   index: home_index,
+                      //   title: home_,
+                      //   itemIndex: 0,
+                      // ),
+                      NavItem(
+                        index: about_index,
+                        title: about_,
+                        itemIndex: 1,
+                      ),
+                      NavItem(
+                        index: experience_index,
+                        title: experience_,
+                        itemIndex: 2,
+                      ),
+                      NavItem(
+                        index: work_index,
+                        title: work_,
+                        itemIndex: 3,
+                      ),
+                      NavItem(
+                        index: contact_index,
+                        title: contact_,
+                        itemIndex: 4,
+                      ),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      OutlinedButton(
+                          style: OutlinedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(4)),
+                            backgroundColor: themeprovider.isDarkMode
+                                ? index_color.withOpacity(0.2)
+                                : light_index_color.withOpacity(0.2),
+                            side: BorderSide(
+                              color: themeprovider.isDarkMode
+                                  ? index_color
+                                  : light_index_color,
+                            ),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 15, vertical: 10),
+                            child: Text(
+                              'Resume',
+                              style: TextStyle(
+                                  fontSize: isMobile(context)
+                                      ? 14
+                                      : isTab(context)
+                                          ? 12
+                                          : 14,
+                                  color: themeprovider.isDarkMode
+                                      ? index_color
+                                      : light_index_color),
+                            ),
+                          ),
+                          onPressed: () async {
+                            // launchResume();
+                            final path = 'resume.pdf';
+
+                            var bytes = await rootBundle
+                                .load(path); // location of your asset file
+
+                            final blob = html.Blob([bytes], 'application/pdf');
+                            final url = html.Url.createObjectUrlFromBlob(blob);
+                            html.window.open(url, "_blank");
+                            html.Url.revokeObjectUrl(url);
+
+                            // final file = await fromAsset(path, "resume.pdf");
+                            // openPDF(context, file);
+                          }),
+                    ],
+                  ),
+                ),
+              ),
+
+            //when device is mobile then the menu icon button is show
+            if (isMobile(context))
+              Expanded(
+                flex: 5,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 20),
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: IconButton(
+                        icon: Icon(
+                          Icons.menu,
+                          color: themeprovider.isDarkMode
+                              ? index_color
+                              : navy_color,
+                        ),
+                        onPressed: () {
+                          Scaffold.of(context).openEndDrawer();
+                        }),
+                  ),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Future launchResume() async {
+  //   await launch('https://brittanychiang.com/resume.pdf');
+  // }
+
+  // Future<File> fromAsset(String asset, String filename) async {
+  //   // To open from assets, you can copy them to the app storage folder, and the access them "locally"
+  //   Completer<File> completer = Completer();
+
+  //   try {
+  //     var dir = await getApplicationDocumentsDirectory();
+  //     File file = File("${dir.path}/$filename");
+  //     var data = await rootBundle.load(asset);
+  //     var bytes = data.buffer.asUint8List();
+  //     await file.writeAsBytes(bytes, flush: true);
+  //     completer.complete(file);
+  //   } catch (e) {
+  //     throw Exception('Error parsing asset file!');
+  //   }
+
+  //   return completer.future;
+  // }
+
+  // void openPDF(BuildContext context, File file) => Navigator.of(context).push(
+  //       MaterialPageRoute(builder: (context) => PDFViewerPage(file: file)),
+  //     );
+}
